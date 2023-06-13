70048848988ffc836899a529fc494d7abeae2b1a
mohannaddQA <qa.mohannad@gmail.com>
2023-06-13 03:47:16 +0000

    m

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -21,7 +21,6 @@ describe("LinkedList", () => {
     expect(mylist.head.value).toBe("b");
   });
 
-  // Test case 4: Can properly insert multiple nodes into the linked list
   test("can properly insert multiple nodes into the linked list", () => {
     const mylist = new LinkedList();
     mylist.insert("a");
@@ -30,7 +29,6 @@ describe("LinkedList", () => {
     expect(mylist.head.value).toBe("c");
   });
 
-  // Test case 5: Will return true when finding a value within the linked list that exists
   test("returns true when finding an existing value in the linked list", () => {
     const mylist = new LinkedList();
     mylist.insert("a");
@@ -39,7 +37,6 @@ describe("LinkedList", () => {
     expect(mylist.includes("b")).toBe(true);
   });
 
-  // Test case 6: Will return false when searching for a value in the linked list that does not exist
   test("returns false when searching for a non-existing value in the linked list", () => {
     const mylist = new LinkedList();
     mylist.insert("a");
@@ -48,7 +45,6 @@ describe("LinkedList", () => {
     expect(mylist.includes("d")).toBe(false);
   });
 
-  // Test case 7: Can properly return a collection of all the values that exist in the linked list
   test("can properly return a collection of all values in the linked list", () => {
     const mylist = new LinkedList();
     mylist.insert("a");
